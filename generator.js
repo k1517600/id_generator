8a35254b1afa86fb18efc4ce895804358b619b48
k1517600 <98120580+k1517600@users.noreply.github.com>
2023-03-30 14:12:46 -0500

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,104 @@
+
+
+class Generator{
+
+
+ 
+
+    constructor(user, pass, url, grade){
+
+        this.user = user;
+
+        this.pass = pass;
+
+        this.url = url;
+
+        this.grade = grade;
+
+    }
+
+
+ 
+
+    toBinary(string) {
+
+        const codeUnits = Uint16Array.from(
+
+          { length: string.length },
+
+          (element, index) => string.charCodeAt(index)
+
+        );
+
+        const charCodes = new Uint8Array(codeUnits.buffer);
+
+     
+
+        let result = "";
+
+        charCodes.forEach((char) => {
+
+          result += String.fromCharCode(char);
+
+        });
+
+        return result;
+
+      }
+
+
+ 
+
+    getImage() {
+
+       
+
+        var dataJson = { "userid": this.user, "pass": this.pass, "grade": this.grade};
+
+        var imageRequest = new XMLHttpRequest();
+
+        imageRequest.open("POST", this.url, false);
+
+        imageRequest.setRequestHeader('Content-Type', 'application/json');
+
+        imageRequest.send(JSON.stringify(dataJson));
+
+        var base64Data = imageRequest.responseText
+
+        console.log(base64Data);
+
+        var image = new Image();
+
+        image.src = 'data:image/png;base64,'+base64Data;
+
+        document.body.appendChild(image);
+
+    }
+
+}
+
+
+ 
+
+var current_ngrok_link = "http://127.0.0.1:5000/GenerateID"
+
+
+ 
+
+function makeGenerator(){
+
+  var id = document.getElementById("id").value;
+
+  var grade = document.getElementById("grade").value;
+
+  var pass = document.getElementById("pass").value;
+
+  var newGen = new Generator(id, pass, current_ngrok_link, grade)
+
+  newGen.getImage()
+
+}
+
+
+
+ 
